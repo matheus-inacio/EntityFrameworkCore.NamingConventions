1831f89c9532ff83796eb0ebfe1739721254d676
matheus.medeiros <29243277+matheus-inacio@users.noreply.github.com>
2023-11-05 21:15:10 -0300

    Fixed test.

--- EFCore.NamingConventions/Rewriters/UpperSnakeCaseNameRewriter.cs
+++ EFCore.NamingConventions/Rewriters/UpperSnakeCaseNameRewriter.cs
@@ -1,7 +1,12 @@
 ﻿namespace EFCore.NamingConventions.Rewriters;
 
-public class UpperSnakeCaseNameRewriter(CultureInfo culture) : SnakeCaseNameRewriter(culture)
+public class UpperSnakeCaseNameRewriter : SnakeCaseNameRewriter
 {
+    private readonly CultureInfo _culture;
+
+    public UpperSnakeCaseNameRewriter(CultureInfo culture) : base(culture)
+        => _culture = culture;
+
     public override string RewriteName(string name)
-        => base.RewriteName(name).ToUpper(culture);
+        => base.RewriteName(name).ToUpper(_culture);
 }
